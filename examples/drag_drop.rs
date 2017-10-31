18d126d793a46c1bfbd969e0c0309f4c76b885c1
alanpoon <alanpoon@linux-eup8.suse>
2017-10-31 14:41:46 +0800

    fixed drag_drop

--- examples/drag_drop.rs
+++ examples/drag_drop.rs
@@ -7,8 +7,8 @@ extern crate cardgame_widgets;
 use conrod::{widget, color, Colorable, Widget, Positionable, Sizeable, Labelable};
 use conrod::backend::glium::glium::{self, glutin, Surface};
 use conrod::event;
-use conrod::widget::envelope_editor::EnvelopePoint;
-use cardgame_widgets::custom_widget::dragdrop_list;
+
+use cardgame_widgets::custom_widget::dragdrop_list::DragDropList;
 
 use std::time::Instant;
 
@@ -20,9 +20,7 @@ widget_ids! {
     }
 }
 pub struct App {
-    last_release: Option<std::time::Instant>,
-    hash: [Option<(conrod::position::Point, color::Color, Option<widget::Id>)>; 25],
-    temp: [Option<(conrod::position::Point, color::Color, Option<widget::Id>)>; 25],
+    hash: Vec<color::Color>,
 }
 #[derive(Clone)]
 pub enum ConrodMessage {
@@ -51,15 +49,8 @@ fn main() {
     let mut old_captured_event: Option<ConrodMessage> = None;
     let mut captured_event: Option<ConrodMessage> = None;
     let sixteen_ms = std::time::Duration::from_millis(100);
-    let mut color_hash = [None; 25];
-    color_hash[0] = Some(([0.0, 0.0], color::DARK_YELLOW, None));
-    color_hash[1] = Some(([0.0, 0.0], color::YELLOW, None));
-    color_hash[2] = Some(([0.0, 0.0], color::DARK_BLUE, None));
-    color_hash[3] = Some(([0.0, 0.0], color::LIGHT_PURPLE, None));
     let mut app = App {
-        hash: color_hash.clone(),
-        temp: color_hash,
-        last_release: Some(std::time::Instant::now()),
+        hash: vec![color::DARK_YELLOW, color::YELLOW, color::DARK_BLUE, color::LIGHT_PURPLE],
     };
 
     'render: loop {
@@ -147,135 +138,10 @@ fn main() {
 
 fn set_widgets(ui: &mut conrod::UiCell, ids: &mut Ids, app: &mut App) {
     widget::Canvas::new().color(color::LIGHT_BLUE).set(ids.master, ui);
-    let mut item0 = dragdrop_list::DragDropList::new(app.hash.len())
+    let j = widget::Canvas::new().w_h(50.0, 50.0);
+    DragDropList::new(&mut app.hash, Box::new(move |v| j.color(v)), 50.0)
         .wh([200.0, 200.0])
         .middle_of(ids.master)
         .set(ids.wraplist, ui);
-    let app_pos = app.hash.clone();
-    let mut k_h_iter = app_pos.iter();
-    let mut c = 0;
-    let mut populated_j_ids = vec![];
-    let floating = widget::Canvas::new().floating(true).w_h(110.0, 150.0).label_color(color::WHITE);
-    floating.down_from(ids.wraplist, 0.0)
-        .title_bar("Blue")
-        .color(color::BLUE)
-        .set(ids.floating_a, ui);
-    let j = widget::Canvas::new().w_h(50.0, 50.0);
-    if let Some(_) = app.last_release.clone() {
-        println!("reseting");
-        while let (Some(item), Some(&Some(k_h))) = (item0.next(ui), k_h_iter.next()) {
-            app.last_release = None;
-            let k = item.set(j.color(k_h.clone().1), 50.0, ui);
-            app.hash[c] = Some((ui.xy_of(k).unwrap(), k_h.clone().1, Some(k)));
-            app.temp[c] = Some((ui.xy_of(k).unwrap(), k_h.clone().1, Some(k)));
-            populated_j_ids.push(k);
-            c += 1;
-        }
-    } else {
-        println!("dragging");
-        while let (Some(item), Some(&Some(k_h))) = (item0.next(ui), k_h_iter.next()) {
-            let k = item.set(j.title_bar("Blue").color(k_h.clone().1).floating(true),
-                             50.0,
-                             ui);
-            populated_j_ids.push(k);
-            c += 1;
-        }
-    }
-    let mut k_h_iter2 = app_pos.iter();
-    let mut populated_j_ids_iter = populated_j_ids.iter();
-    let mut c2 = 0;
-    let mut mouse_point = None;
-    let mut rect_dim = None;
-    while let (Some(&_j_id), Some(&Some(k_h))) = (populated_j_ids_iter.next(), k_h_iter2.next()) {
-        if let Some(mouse) = ui.widget_input(_j_id).mouse() {
-            if mouse.buttons.left().is_down() {
-                mouse_point = Some((c2, mouse.abs_xy()));
-                rect_dim = Some(ui.wh_of(_j_id).unwrap());
-            } else if mouse.buttons.left().is_up() {
-                mouse_point = None;
-                rect_dim = None;
-            }
-        }
 
-        c2 += 1;
-    }
-    if let (Some((c2, m_point)), Some(rect_dim)) = (mouse_point, rect_dim) {
-        let mut _c = 0;
-        for _j in app.hash.iter() {
-            if _c == c2 {
-                _c += 1;
-                continue;
-            }
-            if let &Some((_, _, Some(p_widget))) = _j {
-                let _p_rect = ui.rect_of(p_widget).unwrap();
-                if _p_rect.is_over(m_point) {
-                    break;
-                }
-                _c += 1;
-            }
-
-        }
-        let mut len_of_some = 0;
-        for _i in 0..app.hash.len() {
-            if let None = app.hash[_i] {
-                break;
-            } else {
-                len_of_some += 1;
-            }
-        }
-        let _k = if _c >= len_of_some { c2 } else { _c };
-        if _k != c2 {
-            rearrange(c2, _k, &mut app.temp); //(selected,new,..)
-        }
-    } else {
-        let now = std::time::Instant::now();
-        if let &None = &app.last_release {
-            app.last_release = Some(now);
-        }
-        let temp_c = app.temp.clone();
-        app.hash = temp_c;
-    }
-}
-
-//conrod::position::Point
-fn rearrange<T: Clone>(selected_i: usize,
-                       corrected_i: usize,
-                       hash: &mut [Option<([f64; 2], T, Option<widget::Id>)>; 25]) {
-    let hash_c = hash.clone();
-    for _i in 0..hash.len() {
-        if _i == corrected_i {
-            hash[_i] = match (&hash_c[selected_i], &hash_c[_i]) {
-                (&Some((_, ref a_s, _)), &Some((pos, _, ref k))) => {
-                    Some((pos, a_s.clone(), k.clone()))
-                }
-                _ => None,
-            };
-        }
-        if selected_i < corrected_i {
-            //moved backward ____S__->__C
-            if (_i < corrected_i) & (_i >= selected_i) {
-                //println!("move backward");
-                // ____S~~~~C;
-                hash[_i] = match (&hash_c[_i + 1], &hash_c[_i]) {
-                    (&Some((_, ref a_s, _)), &Some((pos, _, ref k))) => {
-                        Some((pos, a_s.clone(), k.clone()))
-                    }
-                    _ => None,
-                };
-            }
-        } else if selected_i > corrected_i {
-            //moved foward _____C__<-S
-            if (_i <= selected_i) & (_i > corrected_i) {
-                //println!("move forward");
-                // ____C~~~S
-                hash[_i] = match (&hash_c[_i - 1], &hash_c[_i]) {
-                    (&Some((_, ref a_s, _)), &Some((pos, _, ref k))) => {
-                        Some((pos, a_s.clone(), k.clone()))
-                    }
-                    _ => None,
-                };
-            }
-        }
-
-    }
 }
--- src/custom_widget/dragdrop_list.rs
+++ src/custom_widget/dragdrop_list.rs
@@ -1,18 +1,24 @@
 use conrod::{self, widget, Positionable, Widget, color, Ui, UiCell, graph, Sizeable, Dimensions,
              Theme, Rect};
-use conrod::position::Point;
 use std;
 use conrod::position::Scalar;
+use conrod::widget::Canvas;
+use std::fmt::Debug;
+use std::marker::Send;
 /// The type upon which we'll implement the `Widget` trait.
 #[derive(WidgetCommon)]
-pub struct DragDropList {
+pub struct DragDropList<'a, T>
+    where T: Clone + Send + 'a + Debug
+{
     /// An object that handles some of the dirty work of rendering a GUI. We don't
     /// really have to worry about it.
     #[conrod(common_builder)]
     common: widget::CommonBuilder,
     /// See the Style struct below.
     style: Style,
-    num: usize,
+    values: &'a mut Vec<T>,
+    widget_closure: Box<Fn(T) -> Canvas<'a>>,
+    item_width: f64,
 }
 
 #[derive(Copy, Clone, Debug, Default, PartialEq, WidgetStyle)]
@@ -25,10 +31,12 @@ widget_ids! {
 }
 
 /// Represents the unique, cached state for our DragDropList widget.
-pub struct State {
+pub struct State<T> {
     ids: Ids,
-    rects: Vec<[f64; 2]>,
+    temp: Vec<(Option<widget::Id>, T)>,
+    last_release: Option<std::time::Instant>,
     acc_w: f64,
+    mouse_point: Option<(usize, conrod::position::Point)>,
 }
 /// The data necessary for instantiating a single item within a `List`.
 #[derive( Debug)]
@@ -64,8 +72,6 @@ impl<'a> Item<'a> {
                          *(self.first_left_id) = Some(widget_id);
                          w
                      })
-                     
-                     //.parent(self.parent_id)
                 .set(widget_id, ui);
 
 
@@ -74,9 +80,7 @@ impl<'a> Item<'a> {
             if let None = last_id {
                 *(self.first_left_id) = Some(widget_id);
             }
-            widget.and(|w| right_position_item(w, last_id, parent_id, 0.0))
-                     //.parent(self.parent_id)
-                     .set(widget_id, ui);
+            widget.and(|w| right_position_item(w, last_id, parent_id, 0.0)).set(widget_id, ui);
         }
         widget_id
     }
@@ -122,7 +126,9 @@ pub struct Items {
 
 impl Items {
     /// Yield the next `Item` in the list.
-    pub fn next(&mut self, ui: &Ui) -> Option<Item> {
+    pub fn next<T>(&mut self, state: &State<T>, ui: &Ui) -> Option<Item>
+        where T: Clone + Send + 'static + Debug
+    {
 
         let Items { ref mut item_indices,
                     ref mut next_item_indices_index,
@@ -134,22 +140,16 @@ impl Items {
 
 
         // Retrieve the `node_index` that was generated for the next `Item`.
-        let node_index =
-            match ui.widget_graph()
-                      .widget(list_id)
-                      .and_then(|container| container.unique_widget_state::<DragDropList>())
-                      .and_then(|&graph::UniqueWidgetState { ref state, .. }| {
-                                    state.ids
-                                        .items
-                                        .get(*next_item_indices_index)
-                                        .map(|&id| id)
-                                }) {
-                Some(node_index) => {
-                    *next_item_indices_index += 1;
-                    Some(node_index)
-                }
-                None => return None,
-            };
+        let node_index = match state.ids
+                  .items
+                  .get(*next_item_indices_index)
+                  .map(|&id| id) {
+            Some(node_index) => {
+                *next_item_indices_index += 1;
+                Some(node_index)
+            }
+            None => return None,
+        };
 
         match (item_indices.next(), node_index) {
             (Some(i), Some(node_index)) => {
@@ -170,34 +170,46 @@ impl Items {
     }
 }
 
-impl DragDropList {
+impl<'a, T> DragDropList<'a, T>
+    where T: Clone + Send + 'a + Debug
+{
     /// Create a button context to be built upon.
-    pub fn new(num: usize) -> Self {
+    pub fn new(values: &'a mut Vec<T>,
+               widget_closure: Box<Fn(T) -> Canvas<'a>>,
+               item_width: f64)
+               -> Self {
         DragDropList {
             common: widget::CommonBuilder::default(),
             style: Style::default(),
-            num: num,
+            values: values,
+            widget_closure: widget_closure,
+            item_width: item_width,
         }
     }
 }
 
 /// A custom Conrod widget must implement the Widget trait. See the **Widget** trait
 /// documentation for more details.
-impl Widget for DragDropList {
+impl<'a, T> Widget for DragDropList<'a, T>
+    where T: Clone + Send + 'a + 'static + Debug
+{
     /// The State struct that we defined above.
-    type State = State;
+    type State = State<T>;
     /// The Style struct that we defined using the `widget_style!` macro.
     type Style = Style;
     /// The event produced by instantiating the widget.
     ///
     /// `Some` when clicked, otherwise `None`.
-    type Event = Items;
+    type Event = ();
 
     fn init_state(&self, id_gen: widget::id::Generator) -> Self::State {
+        let now = std::time::Instant::now();
         State {
             ids: Ids::new(id_gen),
-            rects: vec![],
+            temp: vec![],
+            last_release: Some(now),
             acc_w: 0.0,
+            mouse_point: None,
         }
     }
 
@@ -210,12 +222,23 @@ impl Widget for DragDropList {
     fn update(self, args: widget::UpdateArgs<Self>) -> Self::Event {
         let widget::UpdateArgs { id, state, rect, mut ui, .. } = args;
         let w = rect.w();
-        let item_idx_range = 0..self.num;
-        if state.ids.items.len() < self.num {
+
+        let item_idx_range = 0..self.values.len();
+        if state.ids.items.len() < self.values.len() {
             let id_gen = &mut ui.widget_id_generator();
-            state.update(|state| state.ids.items.resize(self.num, id_gen));
+            state.update(|state| state.ids.items.resize(self.values.len(), id_gen));
         }
-        let items = Items {
+        let value_c = self.values.clone();
+
+        if state.temp.len() < value_c.len() {
+            for _i in state.temp.len()..value_c.len() {
+                if let Some(_v) = value_c.get(_i) {
+                    state.update(|state| state.temp.push((None, _v.clone())));
+                }
+
+            }
+        }
+        let mut items = Items {
             list_id: id,
             item_indices: item_idx_range.clone(),
             next_item_indices_index: 0,
@@ -224,6 +247,117 @@ impl Widget for DragDropList {
             total_w: w,
             acc_w: 0.0,
         };
-        items
+        let mut c = 0;
+
+        let mut values_c_iter = value_c.iter();
+        if let Some(_) = state.last_release {
+            while let (Some(item), Some(k_h)) =
+                (items.next::<T>(&state, ui), values_c_iter.next()) {
+                let widget = (*self.widget_closure)(k_h.clone());
+                let k = item.set(widget, self.item_width, ui);
+                state.update(|state| {
+                                 if let Some(a) = state.temp.get_mut(c) {
+                                     *a = (Some(k), k_h.clone());
+                                 }
+                                 state.last_release = None;
+                             });
+
+                c += 1;
+            }
+
+        } else {
+            while let (Some(item), Some(k_h)) =
+                (items.next::<T>(&state, ui), values_c_iter.next()) {
+                let widget = (*self.widget_closure)(k_h.clone());
+                let k = item.set(widget.title_bar("Blue"), self.item_width, ui);
+                c += 1;
+            }
+        }
+        let mut c2 = 0;
+        let mut mouse_down = false;
+        {
+            let state_temp_c = state.temp.clone();
+            let mut state_temp_iter = state_temp_c.iter();
+            while let Some(&(Some(_j_id), _)) = state_temp_iter.next() {
+
+                if let Some(mouse) = ui.widget_input(_j_id).mouse() {
+                    if mouse.buttons.left().is_down() {
+                        mouse_down = true;
+                        state.update(|state| { state.mouse_point = Some((c2, mouse.abs_xy())); });
+                    } else if mouse.buttons.left().is_up() {
+                        mouse_down = false;
+                        let now = std::time::Instant::now();
+                        if let &None = &state.last_release {
+                            state.update(|state| state.last_release = Some(now));
+                        }
+                    }
+                }
+                c2 += 1;
+            }
+        }
+        if !mouse_down {
+            if let Some((c2, m_point)) = state.mouse_point {
+
+                let mut _c = 0;
+                for _j in state.temp.iter() {
+                    if _c == c2 {
+                        _c += 1;
+                        continue;
+                    }
+                    if let &(Some(p_widget), _) = _j {
+                        let _p_rect = ui.rect_of(p_widget).unwrap();
+                        if _p_rect.is_over(m_point) {
+                            break;
+                        }
+                        _c += 1;
+                    }
+
+                }
+                let len_of_some = state.temp.len();
+                let _k = if _c >= len_of_some { c2 } else { _c };
+                if _k != c2 {
+                    state.update(|state| { rearrange(c2, _k, &mut state.temp); });
+                    //(selected,new,..)
+
+                    *self.values = state.temp
+                        .iter()
+                        .map(|&(_, ref value)| value.clone())
+                        .collect::<Vec<T>>();
+                }
+                state.update(|state| { state.mouse_point = None; });
+            }
+        }
+
     }
+}
+fn rearrange<T: Clone>(selected_i: usize,
+                       corrected_i: usize,
+                       hash: &mut Vec<(Option<widget::Id>, T)>) {
+    let hash_c = hash.clone();
+    for (_i, ref mut value) in hash.iter_mut().enumerate() {
+        if _i == corrected_i {
+            if let Some(v2) = hash_c.get(selected_i) {
+                value.1 = v2.1.clone();
+            }
+        }
+        if selected_i < corrected_i {
+            //moved backward ____S__->__C
+            if (_i < corrected_i) & (_i >= selected_i) {
+                // ____S~~~~C;
+                if let Some(v2) = hash_c.get(_i + 1) {
+                    value.1 = v2.1.clone();
+                }
+            }
+        } else if selected_i > corrected_i {
+            //moved foward _____C__<-S
+            if (_i <= selected_i) & (_i > corrected_i) {
+                // ____C~~~S
+                if let Some(v2) = hash_c.get(_i - 1) {
+                    value.1 = v2.1.clone();
+                }
+            }
+        }
+
+    }
+
 }
